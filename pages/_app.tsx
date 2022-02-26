5b2780c9ef62742f49ff29cf1d4a758cd32c6d93
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-02-26 15:21:15 +0200

    Move required meta tags to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
@@ -35,20 +34,6 @@ const GlobalStyles = createGlobalStyle`
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#ffffff" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
