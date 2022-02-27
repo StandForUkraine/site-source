a694da4c56ff9b964b547ccad154df2401f22584
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-02-27 21:32:03 +0200

    Added placeholders for every lang

---
+++ texts/pl.ts
@@ -0,0 +1,32 @@
+export default {
+  siteName: `Stand For Ukraine`,
+  siteDescription: `Support Ukraine in the face of Russian military aggression. Donate to verified projects to help Armed Forces of Ukraine or humanitarian charities. Inform the public by sharing our newslets.`,
+  thumbnail: `/thumbnail.png`,
+
+  share: 'Share',
+  donate: 'Donate',
+  inform: 'Inform',
+  donateButton: 'Donate',
+
+  heroHeader: `Help Ukrainian Military & Humanitarian Organizations`,
+  heroText1: `Support Ukraine in the face of Russian military aggression.`,
+  heroText2: `Donate to verified projects to help Armed Forces of Ukraine or humanitarian charities.`,
+  heroText3: `Inform the public by sharing our newslets.`,
+
+  close: 'Close',
+  sharePopupTitle: 'Spread The Cause',
+  sharePopupText1: 'Share this resource to your friends and colleagues.',
+  sharePopupText2: 'Let people know how to support Ukraine.',
+  sharingText: 'Help Ukrainian Military & Humanitarian Organizations #StandForUkraine',
+  copyLink: 'Copy Link',
+
+  // tags
+  All: 'All',
+  Military: 'Military',
+  Medical: 'Medical',
+  Humanitarian: 'Humanitarian',
+  Refugees: 'Refugees',
+  'Non-lethal': 'Non-lethal',
+  'Non-government': 'Non-government',
+  'Human Rights': 'Human Rights',
+}
