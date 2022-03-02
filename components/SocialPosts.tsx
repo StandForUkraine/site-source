58154aa6bf773d3a2922d3c700d6ab3601daf288
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-03-03 00:32:29 +0200

    Fixed image placeholder

--- components/SocialPosts.tsx
+++ components/SocialPosts.tsx
@@ -13,9 +13,7 @@ export const SocialPosts = () => (
             once
             offset={500}
             placeholder={
-              <img
-                width={1200}
-                height={630}
+              <Placeholder
                 data-src={post.image}
                 alt={post.imageAlt}
                 src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
@@ -34,12 +32,21 @@ export default SocialPosts
 
 const PostWrapper = styled.div`
   padding: 20px;
-  max-width: 556px;
+  width: 100%;
   display: inline-block;
+
+  @media (min-width: 768px) {
+    width: 50%;
+  }
 `
 
 const PostImage = styled.img`
-  max-width: 100%;
   width: 100%;
   height: auto;
+`
+
+const Placeholder = styled.img`
+  width: 100%;
+  height: 0;
+  padding-bottom: ${(600 / 1200) * 100}%;
 `
