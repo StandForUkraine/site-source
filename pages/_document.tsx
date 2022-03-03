1510ef1a2c162d6d13498e5b12533dad4e138547
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-03-03 19:43:20 +0200

    Fixed problem with SSG (window is undefined)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { langs } from 'texts'
 
@@ -7,7 +14,7 @@ export const GA_ID = 'G-48DN99WD6W'
 type MyDocumentProps = DocumentInitialProps & {
   origin: string
   styles: JSX.Element
-};
+}
 
 export default class MyDocument extends Document<MyDocumentProps> {
   static async getInitialProps(ctx: DocumentContext) {
@@ -22,15 +29,9 @@ export default class MyDocument extends Document<MyDocumentProps> {
 
       const initialProps = await Document.getInitialProps(ctx)
 
-      /**
-       * Taken from https://github.com/jakeburden/next-absolute-url/blob/master/index.ts
-       */
-      const host = ctx.req?.headers?.host ?? window.location.host;
-      const protocol = host.startsWith('localhost') ? 'http:' : 'https:';
-      
       return {
         ...initialProps,
-        origin: `${protocol}//${host}`,
+        origin: `https://standforukraine.com`,
         styles: (
           <>
             {initialProps.styles}
@@ -72,27 +73,20 @@ export default class MyDocument extends Document<MyDocumentProps> {
             }}
           />
 
-          <link
-            rel="canonical"
-            href={this.props.origin}
-          />
-          
+          <link rel="canonical" href={this.props.origin} />
+
           {/* See: https://developers.google.com/search/docs/advanced/crawling/localized-versions#html */}
-          <link
-            rel="alternate"
-            hrefLang="x-default"
-            href={`${this.props.origin}/`}
-          />
-          {
-            langs.filter((lang) => lang !== 'ua').map((lang) => (
+          <link rel="alternate" hrefLang="x-default" href={`${this.props.origin}/`} />
+          {langs
+            .filter((lang) => lang !== 'ua')
+            .map((lang) => (
               <link
                 key={lang}
                 rel="alternate"
                 hrefLang={lang}
                 href={`${this.props.origin}/${lang === 'en' ? '' : lang}`}
               />
-            ))
-          }
+            ))}
         </Head>
         <body>
           <Main />
