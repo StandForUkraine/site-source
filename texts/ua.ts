263222b2e72ff7063fd203576a131fbad38faebc
pogorielova <nst.pogorielova@gmail.com>
2022-03-05 19:02:02 +0200

    UA

--- texts/ua.ts
+++ texts/ua.ts
@@ -1,6 +1,6 @@
 export default {
   siteName: `Stand For Ukraine`,
-  siteDescription: `Підтримай Українську армію!`,
+  siteDescription: `Допоможи Україні впоратись з військовою аграсією з боку Росії. Роби пожертву у перевірені проекти та підтримай Збройні Сили України і благодійні фонди. Сповісти людей навколо, поділившись нашими матеріалами.`,
   thumbnail: `/thumbnail.png`,
 
   share: 'Поділись',
@@ -10,16 +10,16 @@ export default {
 
   heroHeader: `Підтримай Українську Армію та благодійні організації`,
   heroText1: `Допоможи Україні впоратись з Російською військовою аграсією.`,
-  heroText2: `Зроби пожертву до перевірених проектів аби підтримати Збройні Сили України або благодійні фонди.`,
-  heroText3: `Сповісти людей навколо поділившись нашими матеріалами.`,
+  heroText2: `Роби пожертву у перевірені проекти та підтримай Збройні Сили України і благодійні фонди.`,
+  heroText3: `Сповісти людей навколо, поділившись нашими матеріалами.`,
 
   close: 'Закрити',
   sharePopupTitle: 'Поділись сайтом',
   sharePopupText1: 'Піділись цим ресурсом зі своїми друзями та колегами.',
-  sharePopupText2: 'Дай людям знати як підтримати Україну.',
+  sharePopupText2: 'Допоможи людям дізнатись, як підтримати Україну.',
   sharingText: 'Підримай Українську Армію та благодійні організації #StandForUkraine',
-  copyLink: 'Copy Link',
-  copyLinkDone: 'Copied!',
+  copyLink: 'Скопіювати',
+  copyLinkDone: 'Скопійовано!',
 
   // tags
   All: 'Усі',
@@ -34,22 +34,20 @@ export default {
   'Human Rights': 'Права людини',
 
   // footer
-  footerHeader:
-    'Stand for Ukraine—is an aggregator of reliable organizations that helps to fight the Russian invasion and overcome its consequences.',
-  footerGoals: 'Our Goals',
-  goal1: 'Help the Ukrainian military with weapons, ammunition, and supplies',
-  goal2: 'Recover the veterans & victims of war',
-  goal3: 'Shelter and feed the refugees',
-  goal4: 'Aid children, elders, and other vulnerable groups',
-  goal5: 'Support independent press',
-  footerCreds:
-    'This website is made by IT engineers, managers, designers, writers, translators, journalists, and activists from all over Ukraine & beyond.',
-  footerContact: 'Reach us',
+  footerHeader: 'Stand for Ukraine — це добірка надійних організацій, що допомагає боротися із російським вторгненням та долати його наслідки.',
+  footerGoals: 'Наші цілі',
+  goal1: 'Допомогти українським військовим із забезпеченням зброєю і боєприпасами',
+  goal2: 'Підтримати ветеранів і жертв війни',
+  goal3: 'Надати притулок і нагодувати біженців',
+  goal4: 'Допомогти дітям, літнім людям та вразливим групам населення',
+  goal5: 'Підтримувати незалежні видання',
+  footerCreds: 'Цей сайт створено розробниками, менеджерами, дизайнерами, копірайтерами, перекладачами, журналістами і активістами зі всієї України і світу.',
+  footerContact: 'Зв\'язатись з нами',
 
   // filter
   filterTo: 'La',
   filterPayVia: 'Prin intermediul',
-  
+
   // payment methods
   IBAN: 'IBAN',
   Bitcoin: 'Bitcoin',
