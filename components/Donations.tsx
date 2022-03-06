adb7a1d64596af28ed2810e403972494111d0c32
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2022-03-07 01:40:44 +0200

    Multiple selection for tags and pay methods

--- components/Donations.tsx
+++ components/Donations.tsx
@@ -1,37 +1,78 @@
-import { useState } from 'react'
+import { useState, useMemo, useCallback } from 'react'
 import styled from 'styled-components'
 import { DonationItem } from 'utils/donations'
 import { useLang, useText } from 'utils/lang'
-import { allTags, TagOrAll } from 'utils/tags'
-import ContentTags from './ContentTags'
+import { allTags, Tag } from 'utils/tags'
 import LazyLoad from 'react-lazyload'
 import Button from './Button'
 import { useGtag } from 'hooks/useGtag'
-import PayMethods from './PayMethods'
-import { payMethods, PayMethodWithAll } from 'utils/payMethods'
+import { payMethods, PayMethod } from 'utils/payMethods'
+import MultipleSelection from './MultipleSelection'
 
 export const Donations = ({ donations }: { donations: DonationItem[] }) => {
   const t = useText()
-  const [currentTag, setTag] = useState<TagOrAll>('All')
-  const [currentMethod, setMethod] = useState<PayMethodWithAll>('All')
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+  const [selectedMethods, setSelectedMethods] = useState<PayMethod[]>([])
   const { lang } = useLang()
   const gtag = useGtag()
 
-  donations = donations.map((donation) => ({ ...donation, ...donation.byLang[lang] }))
+  const filteredDonations = useMemo(
+    () =>
+      donations
+        .filter((donation) => {
+          const tagResult =
+            selectedTags.length > 0
+              ? !!donation.tags.find((tag) => selectedTags.indexOf(tag) >= 0)
+              : true
 
-  const filteredDonations = donations.filter((donation) => {
-    const tagResult = currentTag !== 'All' ? donation.tags.includes(currentTag) : true
-    const methodResult =
-      currentMethod !== 'All' ? (donation.payMethods ?? []).includes(currentMethod) : true
+          const methodResult =
+            selectedMethods.length > 0
+              ? !!donation.payMethods.find((method) => selectedMethods.indexOf(method) >= 0)
+              : true
 
-    return tagResult && methodResult
-  })
+          return tagResult && methodResult
+        })
+        .map((donation) => ({ ...donation, ...donation.byLang[lang] })),
+    [donations, selectedTags, selectedMethods, lang]
+  )
+
+  const onTagClick = useCallback(
+    (tag: Tag) => {
+      const newTags =
+        selectedTags.indexOf(tag) >= 0
+          ? selectedTags.filter((t) => t !== tag)
+          : [...selectedTags, tag]
+      setSelectedTags(newTags)
+    },
+    [selectedTags, setSelectedTags]
+  )
+
+  const onMethodClick = useCallback(
+    (method: PayMethod) => {
+      const newMethods =
+        selectedMethods.indexOf(method) >= 0
+          ? selectedMethods.filter((m) => m !== method)
+          : [...selectedMethods, method]
+      setSelectedMethods(newMethods)
+    },
+    [selectedMethods, setSelectedMethods]
+  )
 
   return (
     <>
-      <ContentTags tags={['All', ...allTags]} currentTag={currentTag} onTagChange={setTag} />
+      <MultipleSelection
+        title={t('filterTo')}
+        allOptions={[...allTags]}
+        selectedOptions={selectedTags}
+        onOptionClick={onTagClick}
+      />
 
-      <PayMethods methods={['All', ...payMethods]} active={currentMethod} onChange={setMethod} />
+      <MultipleSelection
+        title={t('filterPayVia')}
+        allOptions={payMethods.filter((m) => m !== 'Western Union')}
+        selectedOptions={selectedMethods}
+        onOptionClick={onMethodClick}
+      />
 
       {filteredDonations.length < 1 && <h1>Nothing found.</h1>}
 
---
+++ components/MultipleSelection.tsx
@@ -0,0 +1,37 @@
+import { useText } from 'utils/lang'
+import Chip from './Chip'
+import ChipsWrapper from './ChipsWrapper'
+import FilterLabel from './FilterLabel'
+
+export interface MultipleSelectionProps<T extends string> {
+  title: string
+  allOptions: T[]
+  selectedOptions: T[]
+  onOptionClick: (value: T) => any
+  toLabel?: (value: T) => string
+}
+
+export default function MultipleSelection<T extends string>({
+  title,
+  allOptions,
+  selectedOptions,
+  onOptionClick,
+  toLabel,
+}: MultipleSelectionProps<T>) {
+  const t: any = toLabel || useText() // it's dirty, but I'm too lazy
+
+  return (
+    <ChipsWrapper>
+      <FilterLabel>{title}</FilterLabel>
+      {allOptions.map((option) => (
+        <Chip
+          key={option}
+          isActive={selectedOptions.indexOf(option) >= 0}
+          onClick={() => onOptionClick(option)}
+        >
+          {t(option)}
+        </Chip>
+      ))}
+    </ChipsWrapper>
+  )
+}
