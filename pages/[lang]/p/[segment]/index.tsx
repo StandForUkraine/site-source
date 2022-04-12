fcfb0a8a6cd00e8a48834a019517b9b04ecd88ba
KonstantinKai <kosyak46@gmail.com>
2022-04-12 21:16:35 +0300

    add new page

--- pages/[lang]/p/[segment]/index.tsx
+++ pages/[lang]/p/[segment]/index.tsx
@@ -1,5 +1,5 @@
-export { getStaticPaths } from 'core/post-page'
 import Page from 'core/post-page'
+export { getStaticPaths } from 'core/post-page'
 export { getStaticProps } from 'pages/p/[segment]'
 
 export default Page
---
+++ pages/[lang]/spread-the-word.tsx
@@ -0,0 +1,6 @@
+import SpreadTheWordPage from 'core/spread-the-word-page'
+
+export { getStaticPaths } from 'core/spread-the-word-page';
+export { getStaticProps } from 'pages/spread-the-word';
+
+export default SpreadTheWordPage;
